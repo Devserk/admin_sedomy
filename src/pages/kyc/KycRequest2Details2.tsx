9853b5d36ea139deeddb392457661d71785a5046
Charys245 <ccharmel11@gmail.com>
2025-05-05 11:18:07 +0100

    Add 'Demande de Kyc' as title of details page

--- src/pages/kyc/KycRequest2Details2.tsx
+++ src/pages/kyc/KycRequest2Details2.tsx
@@ -411,7 +411,8 @@ const KycRequest2Details2 = () => {
           {/* En-tête */}
           <div className="flex justify-between items-start">
             <h1 className="text-3xl font-bold text-gray-800">
-              Détails de la transaction #{data.id}
+              Demande de Kyc 
+              {/* #{data.id} */}
             </h1>
             <button
               onClick={() => navigate(-1)}
